12a1042be7bafc492a5e1895c41e29153e2f4b6e
yuuicnak <yuuicnak@yahoo-corp.jp>
2025-09-03 13:31:44 +0900

    add calc layout

---
+++ app/src/main/java/jp/co/meijou/androids/yuuicnak/MainActivity3.java
@@ -0,0 +1,29 @@
+package jp.co.meijou.androids.yuuicnak;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import jp.co.meijou.androids.yuuicnak.databinding.ActivityMain3Binding;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    private ActivityMain3Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMain3Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
