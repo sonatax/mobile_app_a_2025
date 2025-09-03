170d421f348440f410a91244af2a508fe1836d09
yuuicnak <yuuicnak@yahoo-corp.jp>
2025-09-03 14:45:33 +0900

    add intent buttons

--- app/src/main/java/jp/co/meijou/androids/yuuicnak/MainActivity2.java
+++ app/src/main/java/jp/co/meijou/androids/yuuicnak/MainActivity2.java
@@ -1,5 +1,7 @@
 package jp.co.meijou.androids.yuuicnak;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -24,6 +26,20 @@ public class MainActivity2 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // 明示的intent
+        binding.button3.setOnClickListener(v -> {
+            var intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        // 暗黙的intent
+        binding.button4.setOnClickListener(v -> {
+            var intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.yahoo.co.jp"));
+            startActivity(intent);
         });
     }
 }
