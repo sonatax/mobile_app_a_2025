6abc2a01505a10413dfe469019ca2e9f5ffad87b
yuuicnak <yuuicnak@yahoo-corp.jp>
2025-09-04 13:16:33 +0900

    use okHttp and Moshi

---
+++ app/src/main/java/jp/co/meijou/androids/yuuicnak/MainActivity6.java
@@ -0,0 +1,28 @@
+package jp.co.meijou.androids.yuuicnak;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import jp.co.meijou.androids.yuuicnak.databinding.ActivityMain6Binding;
+
+public class MainActivity6 extends AppCompatActivity {
+    private ActivityMain6Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMain6Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
