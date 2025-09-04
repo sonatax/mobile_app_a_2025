17c040f5cbf92986885928480a75cb2a197bcb8a
yuuicnak <yuuicnak@yahoo-corp.jp>
2025-09-04 14:14:53 +0900

    make getImages method

--- app/src/main/java/jp/co/meijou/androids/yuuicnak/MainActivity6.java
+++ app/src/main/java/jp/co/meijou/androids/yuuicnak/MainActivity6.java
@@ -1,17 +1,29 @@
 package jp.co.meijou.androids.yuuicnak;
 
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.IOException;
+
 import jp.co.meijou.androids.yuuicnak.databinding.ActivityMain6Binding;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity6 extends AppCompatActivity {
     private ActivityMain6Binding binding;
+    private final OkHttpClient okHttpClient = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +35,50 @@ public class MainActivity6 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        binding.buttonChange.setOnClickListener(v -> {
+            binding.progressCircular.setVisibility(View.VISIBLE);
+            var text = binding.editTextText3.getText().toString();
+            // textパラメータをつけたURLの作成
+            var url = Uri.parse("https://placehold.jp/500x500.png")
+                    .buildUpon()
+                    .appendQueryParameter("text", text)
+                    .build()
+                    .toString();
+            // 画像の取得開始
+            getImages(url);
+        });
+    }
+
+    private void getImages(String url) {
+        // リクエスト先に画像URLを指定
+        var request = new Request.Builder()
+                .url(url)
+                .build();
+
+        // 非同期通信でリクエスト
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                // 通信に失敗した時に呼ばれる
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                // sleep
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                // InputStreamをBitmapに変換
+                var bitmap = BitmapFactory.decodeStream(response.body().byteStream());
+                // UIスレッド以外で更新するとクラッシュするので、UIスレッド上で実行させる
+                runOnUiThread(() -> binding.imageView2.setImageBitmap(bitmap));
+                runOnUiThread(() -> binding.progressCircular.setVisibility(View.GONE));
+            }
         });
     }
 }
